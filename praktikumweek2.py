4d5d0e0864753fddaa54f2bfb8965cb766979395
riandhikabagus <riandhikabagus0@gmail.com>
2026-03-10 15:11:10 +0700

    Delete praktikumweek2.py

--- praktikumweek2.py
+++
@@ -1,7 +0,0 @@
-def isKabisat(tahun):  
-    if (tahun % 4 == 0 and tahun % 100 != 0) or (tahun % 400 == 0):
-        return True
-    else:
-        return False
-
-print(isKabisat(int(input("Tahun: "))))
